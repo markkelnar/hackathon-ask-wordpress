69f5ec7e4c4ca857074b8e66f0f4b41e47b4d9a2
Mark Kelnar <markkelnar@gmail.com>
2020-04-24 11:15:05 -0500

    add blog reader

--- skill-get-data.sh
+++ skill-get-data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 ID=`python -mjson.tool .ask/ask-states.json | python -c 'import sys, json; print json.load(sys.stdin)["profiles"]["default"]["skillId"]'`
+mkdir -p skill-package/interactionModels/custom/
 ask smapi get-interaction-model -s "$ID" --stage development -l en-US > skill-package/interactionModels/custom/en-US.json
+cat skill-package/interactionModels/custom/en-US.json
 
